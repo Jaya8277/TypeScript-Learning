85ceaa11f915fb8ed41ff07dee3f4dba3e016bc0
Jaya8277 <jayavish423@gmail.com>
2022-05-25 23:31:59 +0530

    useFetch-assignment

--- useFetch/src/App.tsx
+++ useFetch/src/App.tsx
@@ -14,26 +14,27 @@ type User = {
 }
 function App() {
   const [text, setText] = useState("")
-  const { data, err, loading} = useFetch<User>(URL, {
-    per_page:5,
+  const { data, e, Load} = useFetch<User>(URL, {
+    per_page:9,
     q: text || "Jaya8277"
   })
   console.log(text)
   const onChangeDebounced = useDebouncedCallback((e) => setText(e.target.value),[],1000,0)
   return  (
     <div className="App">
-            <input type="text" onChange={onChangeDebounced}/>
+      <h1>Searh GitHub UserName</h1>
+            <input id="input1" placeholder="Enter The UserName" type="text" onChange={onChangeDebounced}/>
             {  
-            loading ? ( <div><h1>.....Loading</h1></div>
-              ) : err ? (
-              <div>something went wrong</div> 
+            Load ? ( <div><h1>...Load Ho Rha Hai</h1></div>
+              ) : e ? (
+              <div>Kuch To Galat hai</div> 
               ) :
             data.map((user) => {
               return (
-              <div key={user.id}>
+              <div id="box" key={user.id}>
                 <p>{user.id}</p>
                 <h2>{user.login}</h2>
-                <img style={{width:"200px", height:"200px"}} src={user.avatar_url} alt =""/>
+                <img id="image1" src={user.avatar_url} alt =""/>
               </div>
               )
         })} 
--- useFetch/src/hooks/useFetch.ts
+++ useFetch/src/hooks/useFetch.ts
@@ -5,26 +5,25 @@ type paramtype = {
     q:string,
     per_page:number
 }
-//for obj type Record<string, any>
 export const useFetch = <T>(url: string, params?: paramtype) => {
-    const [loading, setLoading] = useState(false)
-    const[err, setError] = useState(false)
+    const [Load, setLoad] = useState(false)
+    const[e, setError] = useState(false)
     const[data, setData] = useState<T[]>([])
     useEffect (() => {
-        setLoading(true)
+        setLoad(true)
         axios.get(url, {
             params: {
                 ...params
             }
         }).then(({data}) => {
-            setLoading(false)
+            setLoad(false)
             setData(data.items);
         }).catch(() => {
-            setLoading(false)
+            setLoad(false)
             setError(true)
         })   
     }, [params?.q])
 
-    return { loading, data, err}
+    return { Load, data, e}
 
 }
