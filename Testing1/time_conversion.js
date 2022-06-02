a0cdea1cd5228e7c919a6d4ed629683ee0208868
Jaya8277 <jayavish423@gmail.com>
2022-06-02 16:55:48 +0530

    time_Conversion

--- Testing1/time_conversion.js
+++ Testing1/time_conversion.js
@@ -1,39 +1,58 @@
 function time(n) {
-    let op, t;
-    if (n < 60000) {
+    let Time, t;
+    if (n < 60000)
+    {
       t = Math.floor(n / 1000);
-      if (t === 1) {
-        op = `${t} second`;
-      } else {
-        op = `${t} seconds`;
+      if (t === 1) 
+    {
+        Time = `${t} second`;
+    } else 
+    {
+        Time = `${t} seconds`;
       }
-    } else if (n >= 60000 && n < 3600000) {
+    } 
+    else if (n >= 60000 && n < 3600000)
+    {
       t = Math.floor(n / 1000);
-      if (t === 60) {
+      if (t === 60) 
+    {
         t = t / 60;
-        op = `${t} minute`;
-      } else if (t > 60 && t % 60 === 0) {
+        Time = `${t} minute`;
+    } 
+    else if (t > 60 && t % 60 === 0) 
+    {
         t = t / 60;
-        op = `${t} minutes`;
-      } else if (t > 60 && t % 60 !== 0) {
+        Time = `${t} minutes`;
+    }
+    else if (t > 60 && t % 60 !== 0) 
+    {
         let m = Math.floor(t / 60);
         let s = t % 60;
-        if (m === 1) {
-          op = `${m} minute ${s} seconds`;
-        } else if (m > 1) {
-          op = `${m} minutes ${s} seconds`;
+        if (m === 1) 
+        {
+          Time = `${m} minute ${s} seconds`;
+        }
+         else if (m > 1) 
+        {
+          Time = `${m} minutes ${s} seconds`;
         }
       }
-    } else if (n >= 3600000) {
+    } 
+    else if (n >= 3600000) 
+    {
       t = n / 1000;
       let h, m, s;
-      if (t === 3600) {
+      if (t === 3600) 
+      {
         t = t / 3600;
-        op = `${t} hour`;
-      } else if (t > 3600 && t % 3600 === 0) {
+        Time = `${t} hour`;
+      } 
+      else if (t > 3600 && t % 3600 === 0) 
+      {
         h = t / 3600;
-        op = `${h} hours`;
-      } else if (t > 3600 && t % 3600 !== 0) {
+        Time = `${h} hours`;
+      } else if (t > 3600 && t % 3600 !== 0) 
+      {
         h = Math.floor(t / 3600);
         m = Math.floor((t % 3600) / 60);
         s = Math.floor((t % 3600) % 60);
@@ -48,10 +67,10 @@ function time(n) {
         } else {
           seconds = "seconds";
         }
-        op = `${h} hours ${m} ${minutes} ${s} ${seconds}`;
+        Time = `${h} hours ${m} ${minutes} ${s} ${seconds}`;
       }
     }
-    return op;
+    return Time;
   }
   
 
