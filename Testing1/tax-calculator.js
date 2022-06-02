55cdfd24b831fc2bb05fa75849b42215c58fb2c3
Jaya8277 <jayavish423@gmail.com>
2022-06-02 16:43:54 +0530

    tax-calculator

--- Testing1/tax-calculator.js
+++ Testing1/tax-calculator.js
@@ -1,17 +1,24 @@
-function tax(n) {
-    if (n < 250000) {
+function taxcalculator(n)
+ {
+    if (n < 250000) 
+    {
       return 0;
-    } else if (n >= 250000 && n < 500000) {
+    } else if (n >= 250000 && n < 500000)
+    {
       n = Math.floor(n / 10);
       return n;
-    } else if (n >= 500000 && n < 1000000) {
+    }
+     else if (n >= 500000 && n < 1000000) 
+    {
       n = Math.floor(n / 5);
       return n;
-    } else if (n >= 1000000) {
+    }
+    else if (n >= 1000000) 
+    {
       n = Math.floor((3 * n) / 10);
       return n;
     }
   }
   
   
-  module.exports = tax;
+  module.exports = taxcalculator;
