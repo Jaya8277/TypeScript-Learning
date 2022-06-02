85428bc95499dec7fed96d31c1da68db7c80a6d2
Jaya8277 <jayavish423@gmail.com>
2022-06-02 16:44:31 +0530

    tax-calculator

--- Testing1/_tests_/tax-calculator.test.js
+++ Testing1/_tests_/tax-calculator.test.js
@@ -1,13 +1,13 @@
    
-const tax = require("../tax-calculator");
+const taxcalculator = require("../tax-calculator");
 
 describe("Testing tax calculation", function () {
   test("should work on all posititive numbers", function () {
-    expect(tax(0)).toBe(0);
-    expect(tax(249999)).toBe(0);
-    expect(tax(250000)).toBe(25000);
-    expect(tax(500000)).toBe(100000);
-    expect(tax(1200000)).toBe(360000);
-    expect(tax(9000000)).toBe(2700000);
+    expect(taxcalculator(0)).toBe(0);
+    expect(taxcalculator(249999)).toBe(0);
+    expect(taxcalculator(250000)).toBe(25000);
+    expect(taxcalculator(500000)).toBe(100000);
+    expect(taxcalculator(1200000)).toBe(360000);
+    expect(taxcalculator(9000000)).toBe(2700000);
   });
 });
